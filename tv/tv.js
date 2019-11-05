889b68ccf868e720634dbf7ecdfe7db4546a877b
xukzhao <39426823+xukzhao@users.noreply.github.com>
2019-11-05 22:58:13 +0800

    Initial commit

--- tv/tv.js
+++ tv/tv.js
@@ -6,7 +6,7 @@ if (isMobile) {
 	document.getElementById('myElement').innerHTML = '<' + 'video autoplay="autoplay" src="' + u + '" controls="controls" width="100%" height="100%" preload="auto"' + '></' + 'video>'
 } else {
     var flashvars = {
-	f: 'http://www.icantv.cn/ckplayer/m3u8.swf',
+	f: 'http://www.tvniu.com/ckplayer/m3u8.swf',
 	a: encodeURIComponent(u),
 	lv: 1,
 	c: 0,
@@ -21,7 +21,7 @@ if (isMobile) {
 		allowScriptAccess: 'always',
 		wmode: 'transparent'
 	};
-	CKobject.embedSWF('http://www.icantv.cn/ckplayer/ckplayer.swf', 'myElement', 'ckplayer_a1', '100%', '100%', flashvars, params)
+	CKobject.embedSWF('http://www.tvniu.com/ckplayer/ckplayer.swf', 'myElement', 'ckplayer_a1', '100%', '100%', flashvars, params)
 }} 
 
 function frame(url){if(url){document.getElementById('myElement').innerHTML='<iframe border="0" marginWidth="0" frameSpacing="0" marginHeight="0" src="'+url+'" frameBorder="0" noResize width="100%" height="100%" vspace="0" scrolling="no"></iframe>';}}
@@ -30,10 +30,14 @@ function m(){
  var	isMobile = /android|phone|mobi|ipad|ipod|blackberry|symbian|midp|ios/i.test(navigator.userAgent.toLowerCase());	
 var m1 = document.getElementById("m1");
 var m2 = document.getElementById("m2");
+var m3 = document.getElementById("m3");
+var m4 = document.getElementById("m4");
 if (isMobile){
 	m2.style.display="none";
+	m4.style.display="none";
 }else {
 m1.style.display="none";
+m3.style.display="none";
 }}
 
 
